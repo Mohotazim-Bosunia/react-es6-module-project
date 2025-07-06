0a9bd2c4ddcb581ea1127da8b3bc1ec7ed0a97a6
Mohotazim-Bosunia <zimhossen0@gmail.com>
2025-07-06 17:30:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Bottles from './Componenets/Bottles/Bottles'
+import Header from './Componenets/Header/Header'
+
+function App() {
+  
+
+  return (
+    <>
+    
+      <Header></Header>
+      <Bottles></Bottles>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Componenets/Bottles/Bottles.jsx
@@ -0,0 +1,10 @@
+
+const Bottles = () => {
+    return (
+        <div>
+           <h2>Wonderful Bottles</h2> 
+        </div>
+    );
+};
+
+export default Bottles;
