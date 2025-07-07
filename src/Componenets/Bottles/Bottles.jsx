e4c40ccfa1e309f1297374952c66a738ddc47af6
Mohotazim-Bosunia <zimhossen0@gmail.com>
2025-07-07 20:21:06 +0600

    sixth commit

--- src/Componenets/Bottles/Bottles.jsx
+++ src/Componenets/Bottles/Bottles.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import './Bottles.css'
+import { addToLS, getStoredCart } from "../../utilities/Localstorage";
 const Bottles = () => {
 
         
@@ -14,6 +15,27 @@ const Bottles = () => {
 
         } , [])
 
+        useEffect(() =>{
+            console.log('called the useEffect',bottles.length)
+            if(bottles.length>0){
+                const storedCart=getStoredCart();
+            console.log(storedCart,bottles);
+            
+            const saveCart=[];
+            for(const id of storedCart){
+                console.log(id)
+                const bottle=bottles.find(bottle =>bottle.id===id);
+            if(bottle){
+                saveCart.push(bottle);
+
+            }
+            console.log('save cart:',saveCart)
+            setCart(saveCart);
+            }
+            }
+           
+        },[bottles])
+
          //console.log(bottles)
         // console.log(typeof bottles)
         
@@ -21,7 +43,7 @@ const Bottles = () => {
             const newCart=[...cart,bottle]
             setCart(newCart)
             //console.log(newCart)
-            
+            addToLS(bottle.id)
         }
 
     return (
